e397db27ef3d1ad0d6788edd46b4e4af861d6a9e
krishna020906 <dracogaming0209@gmail.com>
2026-02-17 14:49:43 +0530

    added marquee section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Hero from './sections/Hero'
 import AnimatedCounter from './components/AnimatedCounter'
 import ShowcaseSection from './sections/ShowcaseSection'
+import Navbar from './components/Navbar'
+import LogoShowcase from './sections/LogoSections'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
   return (
     <>
+      <Navbar/>
       <Hero/>
       <AnimatedCounter/>
       <ShowcaseSection/>
+      <LogoShowcase/>
+      <FeatureCards/>
     </>
   )
 }
